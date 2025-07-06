b47ba699f6cd09faa9259b21c28d7e0a2d95ed64
EmperorAkashi <linchen4869@ucla.edu>
2025-07-06 13:11:35 -0700

    change iter

--- workspace/pnp/pnp_test_template.cpp
+++ workspace/pnp/pnp_test_template.cpp
@@ -45,7 +45,7 @@ bool EstimatorWrapper::runStandalone(const std::vector<Eigen::Vector2d>& points2
     switch (type_) {
         case EstimatorType::EPnP: {
             EPNPEstimator_ estimator;
-            EPNPEstimator_::num_iterations = 10;
+            EPNPEstimator_::num_iterations = 8;
             estimated_extrinsic = estimator.Estimate(points2D, points3D);
             if (residuals) {
                 estimator.Residuals(points2D, points3D, estimated_extrinsic[0], residuals);
@@ -86,6 +86,7 @@ bool EstimatorWrapper::runStandalone(const std::vector<Eigen::Vector2d>& points2
         }
         case EstimatorType::LHM: {
             LHMEstimator::options_.rot_init_est = "horn"; // Set global LHM options
+            LHMEstimator::options_.lhm_iter = 8;
             std::cout << "current ransac option inside estimator is: " << options_.use_ransac << std::endl;
 
             LHMEstimator estimator;
@@ -98,7 +99,7 @@ bool EstimatorWrapper::runStandalone(const std::vector<Eigen::Vector2d>& points2
         case EstimatorType::DRaM_LHM: {
             LHMEstimator::options_.rot_init_est = "dram";
             LHMEstimator::options_.optim_option = "lhm";
-            LHMEstimator::options_.lhm_iter = 5;
+            LHMEstimator::options_.lhm_iter = 8;
 
             LHMEstimator estimator;
             estimated_extrinsic = estimator.Estimate(points2D, points3D);
